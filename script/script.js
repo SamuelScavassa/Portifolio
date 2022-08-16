f1f823a1eb9893cb1af7cfa5615abf2bdf883295
Samuel <87392424+SamuelScavassa@users.noreply.github.com>
2022-08-16 14:01:19 -0300

    commit

---
+++ script/script.js
@@ -0,0 +1,13 @@
+window.sr = ScrollReveal({reset: true});
+
+sr.reveal('.conteudo_index',{
+    delay: 500,
+    duration: 2000,
+    rotate: {x:0 , y:80, z:0}
+});
+
+sr.reveal('footer',{
+    delay:3000,
+    duration:1500
+    
+})
