b4036ddc27a57b0856a649cd142dc81ff5861271
Samuel <87392424+SamuelScavassa@users.noreply.github.com>
2022-08-29 09:43:55 -0300

    commit

---
+++ Portifolio-main/script/index.js
@@ -0,0 +1,34 @@
+window.sr = ScrollReveal({reset: true});
+
+sr.reveal('.conteudo_index',{
+    delay: 500,
+    duration: 2000,
+    rotate: {x:0 , y:80, z:0}
+});
+
+sr.reveal('footer',{
+    delay:3000,
+    duration:1500
+    
+})
+
+sr.reveal('.job',{
+    delay:1000,
+    duration:2000,
+    rotate: {x:80, y:0, z:0}
+})
+
+sr.reveal('.sobre_mim',{
+    delay:1000,
+    duration:1500    
+})
+
+sr.reveal('.disciplinas',{
+    delay:2000,
+    duration:1500    
+})
+
+sr.reveal('.cursos',{
+    delay:2000,
+    duration:1500    
+})
